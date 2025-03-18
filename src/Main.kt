f2ee0d938ffa02fe8f526fb7ee02aeb503ea89b4
Taine <tcsmith2@waimea.school.nz>
2025-03-18 15:10:04 +1300

    project nootes

--- src/Main.kt
+++ src/Main.kt
@@ -8,6 +8,8 @@
  * ---------------------------------------------------------------------
  * Notes: This is a two-player game, played on a one-dimensional grid with coins, where the aim is to win by being the player who removes the gold coin.
  * PROJECT NOTES HERE
+ * Start by asking for each players names (this for the end screen). Each player will be "Player 1" & or "Player 2", however it should show who is what "Player" by their inputted name
+ * Make the coins, and assign them diffrent values (Silver coin - silverCoin1) as there will be multiple coins.
  * =====================================================================
  */
 
